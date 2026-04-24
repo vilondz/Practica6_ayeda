b7c67a24d4f7e18d93e86514f9c39de6a19c59c3
vilondz <alu0101573052@ull.edu.es>
2026-04-24 11:35:08 +0000

    arboles añadidos pero sin completar nodo reworkeado y se supone que el recorrido en inorden deberia funcionar

--- arbolB.h
+++ arbolB.h
@@ -7,6 +7,18 @@ class ArbolB : public NodoB<key> {
 	bool buscar(const key& k) = 0;
 	bool insertar(const key& k) const = 0;
 	void inorden() const;
+	void recorre(std::unique_ptr<NodoB<key>>& nodo);
 	std::ostream& operator<<(std::ostream& out, const ArbolB arbol);
-
 };
+
+template <typename key>
+void ArbolB<key>::inorden()const {
+	recorre(raiz_);
+}
+template<typename key>
+void ArbolB<key>::recorre(std::unique_ptr<NodoB<key>>& nodo){
+	if(nodo == nullptr) return;
+	recorre(nodo->izdo_);
+	std::cout<< nodo->dato_ << "";
+	recorre(nodo->dcho_);
+}
--- nodoB.h
+++ nodoB.h
@@ -5,7 +5,10 @@ class NodoB {
 		key dato_;
 		std::unique_ptr<NodoB<key>> izdo_;
 		std::unique_ptr<NodoB<key>> dcho_;
-		NodoB(const key dato, std::unique_ptr<NodoB<key> izdo = nullptr, std::unique_ptr<NodoB<key>> dcho = nullptr) :
+		NodoB(const key dato, std::unique_ptr<NodoB<key>> izdo = nullptr, std::unique_ptr<NodoB<key>> dcho = nullptr) :
 		dato_(dato), izdo_(izdo), dcho_(dcho){}
+		std::unique_ptr<NodoB<key>>& get_izdo(void){return izdo_;}
+		std::unique_ptr<NodoB<key>>& get_dcho(void){return dcho_;}
+		key& get_dato(){return dato_;}
 };
 
